06eb02e0835e08fd67313030a615541b56fc97c5
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2022-06-14 18:29:10 +0200

    Add files via upload
    finalise les button   realise les formulaire et ajoute les fetch

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Stepper from "./components/Stepper";
+import StepperControl from "./components/StepperControl";
+import { UseContextProvider } from "./contexts/StepperContext";
+
+import VosInformation from "./components/steps/VosInformation";
+import { VotreParours } from "./components/steps/VotreParours";
+
+function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const steps = ["Vos Information", "Votre Parours"];
+
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <VosInformation />;
+
+      case 2:
+        return <VotreParours />;
+
+      default:
+    }
+  };
+
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+
+    direction === "next" ? newStep++ : newStep--;
+    // check if steps are within bounds
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  };
+
+  return (
+    <div className="mx-auto rounded-2xl bg-white pb-2 shadow-xl md:w-1/2">
+      {/* Stepper */}
+      <div className="horizontal container mt-5 ">
+        <Stepper steps={steps} currentStep={currentStep} />
+
+        <div className="my-10 p-10 ">
+          <UseContextProvider>{displayStep(currentStep)}</UseContextProvider>
+        </div>
+      </div>
+
+      {/* navigation button */}
+      {currentStep !== steps.length && (
+        <StepperControl
+          handleClick={handleClick}
+          currentStep={currentStep}
+          steps={steps}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/steps/VotreParours.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import StepperControl from "../StepperControl";
+
+export const VotreParours = () => {
+  return (
+    <div>
+      VotreParours
+      <StepperControl />;
+    </div>
+  );
+};
